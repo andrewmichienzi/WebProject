a021370a2c1b0bc0291973aff677187e125e01fa
michiena <michiena@mail.gvsu.edu>
2016-11-20 14:06:28 -0500

    Adding small sample data which includes users, groups and a task

--- sqlDataMaster.php
+++ sqlDataMaster.php
@@ -13,10 +13,10 @@ header('Content-Type: application/json');
 	//$testUser = array("5", "Andrew", "user1", "andrewmichienzi", "6163894812");
 	//addUser($conn, $testUser);
 	$testTask = array("4", "2", "Task 1", "NULL", "Test Description");
-	addTask($conn, $testTask);
+	//addTask($conn, $testTask);
     	//printUsers($conn);
 	$aResult = array();
-/*
+
     	if( !isset($_POST['functionname']) ) { $aResult['error'] = 'No function name!'; }
 
     	if( !isset($_POST['arguments']) ) { $aResult['error'] = 'No function arguments!'; }
@@ -24,6 +24,9 @@ header('Content-Type: application/json');
     	if( !isset($aResult['error']) ) {
 
         switch($_POST['functionname']) {
+	    case 'Test':
+		$aResult['result'] = 'Worked';
+	    break;
             case 'addUser':
                if( !is_array($_POST['arguments']) || (count($_POST['arguments']) != 6) ) {
                    $aResult['error'] = 'Error in arguments!';
@@ -40,11 +43,11 @@ header('Content-Type: application/json');
                    $aResult['result'] = addUser(floatval($_POST['arguments'][0]), floatval($_POST['arguments'][1]));
                }
 	    case 'addTask'
-               if( !is_array($_POST['arguments']) || (count($_POST['arguments']) != 6) ) {
+               if( !is_array($_POST['arguments']) || (count($_POST['arguments']) != 5) ) {
                    $aResult['error'] = 'Error in arguments!';
                }
                else {
-                   $aResult['result'] = addUser(floatval($_POST['arguments'][0]), floatval($_POST['arguments'][1]));
+                   $aResult['result'] = addTask($_POST['arguments']);
                }
                
             default:
@@ -52,16 +55,16 @@ header('Content-Type: application/json');
                break;
         }
 
-    }*/
+    }
 	function addUser($conn, $args){
-		$sql = "INSERT INTO User (userid, name, username, email, phone) VALUES ('" . $args[0] . "', '" . $args[1] . "', '" . $args[2] . "', '" . $args[3] . "', '" . $args[4] . "');";	
+		$sql = "INSERT INTO Users (userid, name, username, email, phone) VALUES ('" . $args[0] . "', '" . $args[1] . "', '" . $args[2] . "', '" . $args[3] . "', '" . $args[4] . "');";	
 		$retval = mysql_query($sql, $conn);
 		if(! $retval){
 			die('Addng User issue' . mysql_error());
 		}
 	}
 	function printUsers($conn){
-		$sql = "Select * from User;";
+		$sql = "Select * from Users;";
 		$retVal = mysql_query($sql, $conn);
 		while($row = mysql_fetch_array($retVal)){
 			echo $row['username'];
@@ -69,7 +72,7 @@ header('Content-Type: application/json');
 		}
 	}
 	function addTask($conn, $args){
-		$sql = "INSERT INTO Task (taskId, groupId, name, userId, description) VALUES ('" . $args[0] . "', '" . $args[1] . "', '" . $args[2] . "', '" . $args[3] . "', '" . $args[4] . "');";	
+		$sql = "INSERT INTO Tasks (taskId, groupId, name, userId, description) VALUES ('" . $args[0] . "', '" . $args[1] . "', '" . $args[2] . "', '" . $args[3] . "', '" . $args[4] . "');";	
 		$retVal = mysql_query($sql, $conn);
 		if (! $retVal){
 			die('Adding Task Issue  ' . mysql_error());
