b5a44f0630ebfa96b5ee4ac668ff02759e253322
algermo <algermo@mail.gvsu.edu>
2016-12-02 14:44:30 -0500

    working message board

--- MessageBoard/getPosts.php
+++ MessageBoard/getPosts.php
@@ -12,7 +12,7 @@
 		die("Connection failed: " . $conn->connect_error);
 	
 	
-	$sql = "SELECT * FROM MessageBoard WHERE groupId=" .$groupId. ";";
+	$sql = "SELECT * FROM MessageBoard WHERE groupId=" .$groupId. " ORDER BY postDate DESC;";
 	$result = mysql_query($sql, $conn);
 	
 	if($result == NULL) {
@@ -20,24 +20,17 @@
 	}
 	
 	$allPosts = array();
-	$postData = array();
 	
 	while($row = mysql_fetch_array($result)) {
 		$allPosts[$row['messageId']] = array();
-		$postData = $allPosts[$row['messageId']];
-		echo $row['messageId'];
 		$allPosts[$row['messageId']]['date'] = $row['postDate'];
-		echo $allPosts[$row['messageId']]['date'];
+		$allPosts[$row['messageId']]['message'] = $row['message'];
 
-		$postData['messageId'] = $row['messageId'];
-		
-		$postData['date'] = $row['postDate'];
-		$postData['message'] = $row['message'];
 		$userID = $row['userId'];
 		$sql2 = "SELECT name FROM Users WHERE userId = '" .$userID. "';";
 		$result2 = mysql_query($sql2, $conn);
-		while($row = mysql_fetch_array($result2)) {
-			$postData['user'] = $row['name'];
+		while($row2 = mysql_fetch_array($result2)) {
+			$allPosts[$row['messageId']]['user'] = $row2['name'];
 		}
 	}
 	
